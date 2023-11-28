85b65479033443eb3532d312384388997a71f6da
redwan528 <redwankhan144@gmail.com>
2023-11-28 02:00:51 -0500

    Initial Commit

---
+++ AI Voice Assistant/AI_Voice_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Voice_AssistantApp.swift
+//  AI Voice Assistant
+//
+//  Created by Redwan Khan on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AI_Voice_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
